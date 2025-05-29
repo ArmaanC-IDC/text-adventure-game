b5cc378b11527debffc978287e905cbc1cfd0e8b
Ian Luk <iluk@bayviewglen.ca>
2025-05-29 08:59:05 -0400

    Golden Apple Test and Armour

--- src/Item/Passive/Armour.java
+++ src/Item/Passive/Armour.java
@@ -10,19 +10,11 @@ public class Armour extends Item{
     public static Armour createArmour(String type){
         switch (type.toLowerCase()) {
             case "leathersuit":
-<<<<<<< HEAD
-                return new Armour("leathersuit", 1);
-            case "chainsuit":
-                return new Armour("chainsuit", 2);
-            case "diamondsuit":
-                return new Armour("diamondsuit", 3);
-=======
                 return new Armour("leathersuit", ItemLoader.getItemConfig("leathersuit", "resistance"));
             case "chainsuit":
                 return new Armour("chainsuit", ItemLoader.getItemConfig("chainsuit", "resistance"));
             case "diamondsuit":
                 return new Armour("diamondsuit", ItemLoader.getItemConfig("diamondsuit", "resistance"));
->>>>>>> 124160eb0932ecb4034499a9ba8bed5569867faf
             default:
                 return new Armour("UNIMPLEMENTED: " + type, 0);
         }
